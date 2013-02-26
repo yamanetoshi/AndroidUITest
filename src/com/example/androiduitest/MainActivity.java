231f2f9eda51b53d2c54e40da9380dd44467e7c4
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-02-26 16:47:59 +0900

    implement Android test

--- src/com/example/androiduitest/MainActivity.java
+++ src/com/example/androiduitest/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.androiduitest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private int state;
+	
+	public int getState() { return state; }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button = (Button)findViewById(R.id.send);
+        button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(MainActivity.this, SecondAndroidActivity.class);
+//                 startActivityForResult(intent, 100);
+             }
+        });
     }
 
     @Override
@@ -28,4 +42,15 @@ public class MainActivity extends Activity {
         text.setText(edit.getText());
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        state = 1;
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        state = 2;
+    }
 }
